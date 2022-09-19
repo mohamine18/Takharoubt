77cbb33bdf7ab0e38005a833bb651dd0394e1253
amine <mohamine19@gmail.com>
2022-09-19 10:40:34 +0100

    get webhook configured

--- controllers/webhook.js
+++ controllers/webhook.js
@@ -1,4 +1,14 @@
 exports.getWebhook = (req, res) => {
-  console.log(req);
-  res.status(200).send("EVENT_RECEIVED");
+  console.log(req.query);
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+  if (mode && token) {
+    if (mode === "subscribe" && token === process.env.VERIFY_TOKEN) {
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(403);
+    }
+  }
 };
