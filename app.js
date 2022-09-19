44acf4f266735218e57e436876fd89803e01aadd
amine <mohamine19@gmail.com>
2022-09-19 10:25:49 +0100

    bug fixed

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const webhookRouter = require("./routes/webhook");
+const homeRouter = require("./routes/home");
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use("/", homeRouter);
 app.use("/webhook", webhookRouter);
 
 module.exports = app;
