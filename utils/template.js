3aeebfc6921cc9edea9b978389452c40c60001ea
amine <mohamine19@gmail.com>
2022-09-21 12:21:56 +0100

    pictures bug fixed

--- utils/template.js
+++ utils/template.js
@@ -1,14 +1,14 @@
 const image_url = [
-  "https://images.pexels.com/photos/36704/pexels-photo.jpg",
-  "https://images.pexels.com/photos/7249183/pexels-photo-7249183.jpeg",
-  "https://images.pexels.com/photos/8164742/pexels-photo-8164742.jpeg",
-  "https://images.pexels.com/photos/7249185/pexels-photo-7249185.jpeg",
-  "https://images.pexels.com/photos/10206732/pexels-photo-10206732.jpeg",
-  "https://images.pexels.com/photos/6873647/pexels-photo-6873647.jpeg",
-  "https://images.pexels.com/photos/10336570/pexels-photo-10336570.jpeg",
-  "https://images.pexels.com/photos/8522573/pexels-photo-8522573.jpeg",
-  "https://images.pexels.com/photos/7300900/pexels-photo-7300900.jpeg",
-  "https://images.pexels.com/photos/7654916/pexels-photo-7654916.jpeg",
+  "https://images.pexels.com/photos/36704/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/7249183/pexels-photo-7249183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/8164742/pexels-photo-8164742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/7249185/pexels-photo-7249185.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/10206732/pexels-photo-10206732.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/6873647/pexels-photo-6873647.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/10336570/pexels-photo-10336570.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/8522573/pexels-photo-8522573.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/7300900/pexels-photo-7300900.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/7654916/pexels-photo-7654916.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
 ];
 
 // const buttons = [
