664a04e22e91f9b9b66cc0754fb2c37e2328db8e
amine <mohamine19@gmail.com>
2022-09-28 16:23:37 +0100

    dashboard fixed

--- controllers/viber.js
+++ controllers/viber.js
@@ -30,28 +30,25 @@ bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
   }
   response.send(
     new KeyboardMessage({
-      keyboard: {
-        DefaultHeight: false,
-        BgColor: "#FFFFFF",
-        Buttons: [
-          {
-            Columns: 6,
-            Rows: 1,
-            BgColor: "#2db9b9",
-            ActionType: "open-url",
-            ActionBody: `${process.env.WEBSITE_URL}/create-a-room?psid=${response.userProfile.id}&platform=viber`,
-            OpenURLType: "internal",
-            InternalBrowser: {
-              Mode: "fullscreen",
-              CustomTitle: text.createRoom,
-            },
-            Text: text.createRoom,
-            TextVAlign: "middle",
-            TextHAlign: "center",
-            TextSize: "regular",
+      Type: "keyboard",
+      Buttons: [
+        {
+          Columns: 6,
+          Rows: 1,
+          BgColor: "#2db9b9",
+          ActionType: "open-url",
+          ActionBody: `${process.env.WEBSITE_URL}/create-a-room?psid=${response.userProfile.id}&platform=viber`,
+          OpenURLType: "internal",
+          InternalBrowser: {
+            Mode: "fullscreen",
+            CustomTitle: text.createRoom,
           },
-        ],
-      },
+          Text: text.createRoom,
+          TextVAlign: "middle",
+          TextHAlign: "center",
+          TextSize: "regular",
+        },
+      ],
     })
   );
 });
