0a7c7eaed4d201e0709602aa8811bcc5d4bea483
amine <mohamine19@gmail.com>
2022-09-28 16:14:33 +0100

    repalced the keyboard object

--- controllers/viber.js
+++ controllers/viber.js
@@ -4,7 +4,7 @@ const TextMessage = require("viber-bot").Message.Text;
 const KeyboardMessage = require("viber-bot").Message.Keyboard;
 const UrlMessage = require("viber-bot").Message.Url;
 
-const keyboardJson = require("../utils/keyboardViber");
+const text = require("./text");
 
 const bot = new ViberBot({
   authToken: process.env.VIBER_AUTH_TOKEN,
@@ -28,7 +28,32 @@ bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
   if (!(message instanceof TextMessage)) {
     say(response, `Sorry. I can only understand text messages.`);
   }
-  response.send(new KeyboardMessage(keyboardJson(response.userProfile.id)));
+  response.send(
+    new KeyboardMessage({
+      keyboard: {
+        DefaultHeight: false,
+        BgColor: "#FFFFFF",
+        Buttons: [
+          {
+            Columns: 6,
+            Rows: 1,
+            BgColor: "#2db9b9",
+            ActionType: "open-url",
+            ActionBody: `${process.env.WEBSITE_URL}/create-a-room?psid=${response.userProfile.id}&platform=viber`,
+            OpenURLType: "internal",
+            InternalBrowser: {
+              Mode: "fullscreen",
+              CustomTitle: text.createRoom,
+            },
+            Text: text.createRoom,
+            TextVAlign: "middle",
+            TextHAlign: "center",
+            TextSize: "regular",
+          },
+        ],
+      },
+    })
+  );
 });
 
 bot.onTextMessage(/./, (message, response) => {
