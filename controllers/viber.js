1db6ba7e743a71b20f261812c4fbde48dab9a801
amine <mohamine19@gmail.com>
2022-09-29 16:19:19 +0100

    pormise added to send function in viber

--- controllers/viber.js
+++ controllers/viber.js
@@ -14,49 +14,52 @@ const bot = new ViberBot({
 });
 
 function say(response, message) {
-  setTimeout(() => {
-    response.send(new TextMessage(message));
-    response.send(new KeyboardMessage(keyboardJson(response.userProfile.id)));
-  }, 1500);
+  response.send(new KeyboardMessage(keyboardJson(response.userProfile.id)));
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      response.send(new TextMessage(message));
+      resolve("success");
+    }, 1500);
+  });
 }
 
-bot.onSubscribe((response) => {
+bot.onSubscribe(async (response) => {
   // keyboard(response);
-  say(response, text.marhaba);
-  say(response, text.menu);
+  await say(response, text.marhaba);
+  await say(response, text.menu);
 });
 
 bot.onConversationStarted((userProfile, isSubscribed, context, onFinish) => {
   onFinish(new TextMessage(text.marhaba, keyboardJson(userProfile.id)));
 });
 
-bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
+bot.on(BotEvents.MESSAGE_RECEIVED, async (message, response) => {
   // keyboard(response);
   // This sample bot can answer only text messages, let's make sure the user is aware of that.
   if (!(message instanceof TextMessage)) {
-    say(response, `Sorry. I can only understand text messages.`);
+    await say(response, `Sorry. I can only understand text messages.`);
   }
 });
 
-bot.onTextMessage(/./, (message, response) => {
+bot.onTextMessage(/./, async (message, response) => {
   const receivedWord = message.text;
   console.log(receivedWord.match(/^[h](ttps|ttp)/)?.input);
   switch (receivedWord.toLowerCase()) {
     case "hello":
-      say(response, text.marhaba);
-      say(response, text.menu);
+      await say(response, text.marhaba);
+      await say(response, text.menu);
       break;
     case "joinroom":
-      say(response, text.enterRoomCode);
+      await say(response, text.enterRoomCode);
       break;
     case "help":
-      say(response, text.help);
-      say(response, text.how);
+      await say(response, text.help);
+      await say(response, text.how);
       break;
     case receivedWord.match(/^[h](ttps|ttp)/)?.input:
       break;
     default:
-      say(response, text.default);
+      await say(response, text.default);
       break;
   }
 });
