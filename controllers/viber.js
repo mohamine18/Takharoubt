71a2b222e12ad45afd540c4d667d1fc6c8752353
amine <mohamine19@gmail.com>
2022-09-29 09:58:18 +0100

    set min version api for viber

--- controllers/viber.js
+++ controllers/viber.js
@@ -18,40 +18,46 @@ function say(response, message) {
 
 function keyboard(response) {
   response.send(
-    new KeyboardMessage({
-      Type: "keyboard",
-      min_api_version: 7,
-      Buttons: [
-        {
-          Columns: 6,
-          Rows: 1,
-          BgColor: "#009000",
-          Silent: true,
-          ActionType: "open-url",
-          OpenURLType: "internal",
-          InternalBrowser: {
-            Mode: "fullscreen",
-            CustomTitle: text.createRoom,
+    new KeyboardMessage(
+      {
+        Type: "keyboard",
+        min_api_version: 7,
+        Buttons: [
+          {
+            Columns: 6,
+            Rows: 1,
+            BgColor: "#009000",
+            Silent: true,
+            ActionType: "open-url",
+            OpenURLType: "internal",
+            InternalBrowser: {
+              Mode: "fullscreen",
+              CustomTitle: text.createRoom,
+            },
+            ActionBody: `${process.env.WEBSITE_URL}/create-a-room?psid=${response.userProfile.id}&platform=viber`,
+            Text: `<font color="#ffffff">${text.createRoom}</font>`,
+            TextVAlign: "middle",
+            TextHAlign: "center",
+            TextSize: "medium",
           },
-          ActionBody: `${process.env.WEBSITE_URL}/create-a-room?psid=${response.userProfile.id}&platform=viber`,
-          Text: `<font color="#ffffff">${text.createRoom}</font>`,
-          TextVAlign: "middle",
-          TextHAlign: "center",
-          TextSize: "medium",
-        },
-        {
-          Columns: 6,
-          Rows: 1,
-          BgColor: "#009000",
-          ActionType: "reply",
-          ActionBody: `${text.enterRoomCode}`,
-          Text: `<font color="#ffffff">${text.joinRoom}</font>`,
-          TextVAlign: "middle",
-          TextHAlign: "center",
-          TextSize: "medium",
-        },
-      ],
-    })
+          {
+            Columns: 6,
+            Rows: 1,
+            BgColor: "#009000",
+            ActionType: "reply",
+            ActionBody: `${text.enterRoomCode}`,
+            Text: `<font color="#ffffff">${text.joinRoom}</font>`,
+            TextVAlign: "middle",
+            TextHAlign: "center",
+            TextSize: "medium",
+          },
+        ],
+      },
+      null,
+      null,
+      null,
+      7
+    )
   );
 }
 
